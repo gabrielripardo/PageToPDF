240e07683194856c1187518aa9ef629a8435926b
Gabriel R <gts.senna@gmail.com>
2019-01-04 20:51:45 -0200

    Versão Estável - PageToPDF v.1.0

---
+++ getPage.js
@@ -0,0 +1,97 @@
+const puppeteer = require('puppeteer')            
+
+async function openPage(url){
+    const browser = await puppeteer.launch();   //Modo handler
+    //const browser = await puppeteer.launch({ headless: false });     //Modo browser chrome nativo
+    var page = await browser.newPage();         
+    await page.goto(url);    
+
+    inserirCokies(page, browser, url);
+}
+async function inserirCokies(page, browser, url){
+    const cookies = [{
+        'name': '_fbp',
+        'value': 'fb.1.1546445635572.1436825322'
+    },{
+        'name': '_ga',
+        'value': 'GA1.2.42711130.1546445613'
+    },{
+        'name': '_gat_UA2484496511',
+        'value': '1'
+    },{
+        'name': '_gat_UA248449652',
+        'value': '1'
+    },{
+        'name': '_gid',
+        'value': 'GA1.2.285114252.1546445613'
+    },{
+        'name': 'cto_lwid',
+        'value': 'ab20bc42-1b74-4810-8289-5d3816ade5e0'
+    },{
+        'name': 'first_accessed_page',
+        'value': 'https://www.passeidireto.com/'
+    },{
+        'name': 'session_key',
+        'value': 'ad6d7ed817fd15b6194952b031eb9079'
+    }];
+    
+    await page.setCookie(...cookies);
+    const cookiesSet = await page.cookies(url);
+    await page.goto(url);
+    console.log(JSON.stringify(cookiesSet));
+    
+    goByPage(page, browser);
+}
+async function runMouse(page){
+    var cont = 0;
+    while(cont < 1000){
+        await page.mouse.move(100, 100);        
+        await page.mouse.move(200, 200);             
+        await page.waitFor(1000);              
+        console.log(cont++);
+    }
+}
+async function goByPage(page, browser){          
+    runMouse(page); //Simula o movimento do mouse        
+    
+    //Espera o seletor ser carregado
+    await page.waitForSelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-file-contents > div > div > div.file-viewer-navigator > div > div.pages-info > input[type="number"]')
+    
+    //Contadores de páginas
+    var nPage = 1;
+    var nPageMax = 7;
+    while(nPage <= nPageMax){                    
+        
+        //Clica no campo
+        await page.click('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-file-contents > div > div > div.file-viewer-navigator > div > div.pages-info > input[type="number"]');   
+        //Apaga e inseri o nº da pág. e depois 3 segundos vai para próxima.        
+        await page.keyboard.press('Backspace');        
+        await page.keyboard.type(nPage.toString());
+        await page.waitFor(3000);     // Tempo para que que todos os elemento da página carrege      
+        getPage(page, nPage);
+        await page.waitFor(2000);   // Tempo para que o método getPage gere o pdf.        
+        nPage++;
+    }    
+    closePage(browser);
+}
+async function getPage(page, nPage){
+    await page.emulateMedia('screen');
+    var path = 'pag. '+ nPage.toString()+'.pdf';
+    //await page.screenshot({path: path});
+    await page.pdf({path: path, format: 'A4'}); 
+     
+    //openSecondaryPage(); //Abre uma janela da página no chrome.  
+}     
+async function closePage(browser){
+    await browser.close(); 
+    console.log("Procedimento finalizado!");    
+} 
+async function openSecondaryPage(){
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    page.on('load', () => console.log('=====Page loaded!====='));
+    await page.goto('https://google.com');
+}
+
+openPage('https://www.passeidireto.com/arquivo/29471837/avaliando-aprendizado-estrutura-de-dados');
