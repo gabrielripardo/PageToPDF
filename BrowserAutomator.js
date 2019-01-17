65ca0d98eaed4798c723137700881d52c7b6cfc6
Gabriel R <gts.senna@gmail.com>
2019-01-17 11:45:17 -0200

    Print mais rápido das páginas

---
+++ BrowserAutomator.js
@@ -0,0 +1,172 @@
+const puppeteer = require('puppeteer')            
+var merge = require('easy-pdf-merge');
+
+class BrowserAutomator{
+    constructor(formato, interval){
+        this.url = '';
+        this.titulo = '';
+        this.numMaxPages = '';
+        this.formato = formato;
+        this.interval = interval;        
+    }
+    
+    async openPage(url){
+        const browser = await puppeteer.launch();   //Modo handler
+        this.url = url;
+        //const browser = await puppeteer.launch({ headless: false });     //Modo browser chrome nativo
+        var page = await browser.newPage();         
+        await page.goto(url);    
+        
+        this.titulo = await page.title();
+        console.log(this.titulo);        
+        this.getTotalPages(page);    
+        this.inserirCokies(page, browser, url);
+    }    
+    async getTotalPages(page){
+        const element = await page.$(".page-count");
+        const text = await page.evaluate(element => element.textContent, element);
+        var numString = text.replace(/[^0-9]/g,'');             
+        this.numMaxPages = parseInt(numString);          
+    }
+    async inserirCokies(page, browser, url){
+        const cookies = [{
+            'name': '_fbp',
+            'value': 'fb.1.1546445635572.1436825322'
+        },{
+            'name': '_ga',
+            'value': 'GA1.2.42711130.1546445613'
+        },{
+            'name': '_gat_UA2484496511',
+            'value': '1'
+        },{
+            'name': '_gat_UA248449652',
+            'value': '1'
+        },{
+            'name': '_gid',
+            'value': 'GA1.2.285114252.1546445613'
+        },{
+            'name': 'cto_lwid',
+            'value': 'ab20bc42-1b74-4810-8289-5d3816ade5e0'
+        },{
+            'name': 'first_accessed_page',
+            'value': 'https://www.passeidireto.com/'
+        },{
+            'name': 'session_key',
+            'value': 'ad6d7ed817fd15b6194952b031eb9079'
+        }];
+        
+        await page.setCookie(...cookies);
+        const cookiesSet = await page.cookies(url);
+        await page.goto(url);
+     //   console.log(JSON.stringify(cookiesSet));
+        console.log("Number pages: ", this.numMaxPages);
+
+        this.openTabs(page, browser);        
+    }
+    async openTabs(page, browser){
+        this.modifyElements(page, browser, 1);
+                
+        for(var pageStart=1; pageStart<=this.interval; pageStart++){
+            var pageSecond = page;        
+            pageSecond = await browser.newPage();                
+            await pageSecond.goto(this.url);        
+            this.modifyElements(pageSecond, browser, pageStart);            
+        }        
+    }  
+    async modifyElements(page, browser, startPage){
+        this.runMouse(page); //Simula o movimento do mouse   
+        
+        //Elementos poluidores
+        await page.waitForSelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.layout.mv-material-viewer-toolbar.align-center.justify-space-between.row.fill-height.mv-viewer-toolbar');
+        await page.waitForSelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-content-viewer-limitation.mv-file-content-limitation > div');
+        await page.waitForSelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-material-viewer-container.mv-file-infobar > div.layout.mv-material-viewer-infobar.row.justify-end.wrap.align-end.align-center.visible.absolute');
+        await page.waitForSelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-material-viewer-container.mv-file-infobar > div.pd-collapse.vertical.show');    
+            
+        await page.evaluate(() => {        
+            let topBar = document.querySelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.layout.mv-material-viewer-toolbar.align-center.justify-space-between.row.fill-height.mv-viewer-toolbar');
+            let rodape = document.querySelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-content-viewer-limitation.mv-file-content-limitation > div');        
+            let barName = document.querySelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-material-viewer-container.mv-file-infobar > div.layout.mv-material-viewer-infobar.row.justify-end.wrap.align-end.align-center.visible.absolute');
+            let barBackground = document.querySelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-material-viewer-container.mv-file-infobar > div.pd-collapse.vertical.show');        
+            
+            topBar.parentNode.removeChild(topBar);
+            rodape.parentNode.removeChild(rodape);
+            barName.parentNode.removeChild(barName);
+            barBackground.parentNode.removeChild(barBackground);
+
+            //rodape.innerHTML = "ABC"; //Inseri html no elemento
+        });  
+
+        this.goByPage(page, browser, startPage); 
+    }
+    async runMouse(page){
+        var cont = 0;
+        while(cont < 1000){
+            await page.mouse.move(100, 100);        
+            await page.mouse.move(200, 200);             
+            await page.waitFor(1000);              
+            console.log(cont++);
+        }
+    }      
+
+    async goByPage(page, browser, nPage){              
+        //Espera o seletor ser carregado
+        await page.waitForSelector('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-file-contents > div > div > div.file-viewer-navigator > div > div.pages-info > input[type="number"]')
+        
+        //Contadores de páginas                
+        var nPageMax = this.numMaxPages;
+        var qtdBackspace = 1;
+        var tenMultiple = 10;
+        while(nPage <= nPageMax){                    
+            
+            //Clica no campo
+            await page.click('#app > div.application--wrap > main > div > div > div > div > div.layout.align-space-around.justify-space-around.row.fill-height > div.flex.mv-material-viewer-main > div.mv-file.mv-content.limitation-bar > div.mv-file-contents > div > div > div.file-viewer-navigator > div > div.pages-info > input[type="number"]');   
+            
+            //Apaga e inseri o nº da pág. e depois 3 segundos vai para próxima.        
+            for(var nPress=1; nPress<=qtdBackspace; nPress++){
+                await page.keyboard.press('Backspace');        
+            }
+            
+            await page.keyboard.type(nPage.toString());
+            await page.waitFor(3000);     // Tempo para que que todos os elemento da página carrege      
+            this.getPage(page, nPage);
+            await page.waitFor(2000);   // Tempo para que o método getPage gere o pdf.        
+            
+            if(nPage==tenMultiple){
+                qtdBackspace++;
+                tenMultiple = tenMultiple*10;
+            }
+            nPage = nPage + this.interval;
+        }    
+        if(nPage-this.interval == nPageMax){
+           this.mergePDFs(this.titulo, nPageMax, browser);
+        }        
+    }
+    async getPage(page, nPage){
+        await page.emulateMedia('screen');
+        var path = 'temp/pag. '+ nPage.toString()+'.pdf';
+        //await page.screenshot({path: path});
+        await page.pdf({path: path, format: this.formato});              
+    }     
+    async closePage(browser){
+        await browser.close(); 
+        console.log("Procedimento finalizado!");    
+    } 
+    mergePDFs(titlePage, nMaxPage, browser){        
+        var dest_file = titlePage+'.pdf';
+        var nPagesMax = nMaxPage;
+        var source_files = [];
+        for(var n=1; n<=nPagesMax; n++){
+            source_files.push('temp/pag. '+ n +'.pdf');
+        }
+        console.log(source_files);
+
+        merge(source_files,dest_file,function(err){
+            if(err)
+            return console.log(err);
+            console.log('Successfully merged pages!');
+        });
+        this.closePage(browser);
+    }
+}
+module.exports = BrowserAutomator;
+
